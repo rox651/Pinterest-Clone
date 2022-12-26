7b140bb5bb4be444e253540143e04622d82825a1
Homero José <96498455+rox651@users.noreply.github.com>
2022-12-26 11:40:36 -0500

    images size

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -13,15 +13,14 @@ export const Posts = () => {
    return (
       <>
          <section className="relative  min-h-screen ">
-            <div className="px-5  max-w-7xl mx-auto   grid grid-cols-2 lg:grid-cols-3  gap-3 ">
+            <div className="px-5  max-w-7xl mx-auto   grid grid-cols-2 lg:grid-cols-3   gap-3 ">
                {data.map(post => {
                   return (
-                     <div className="overflow-hidden w-full h-full">
+                     <div key={post?.slug?.current} className="overflow-hidden w-full h-full">
                         <LazyLoadImage
-                           key={post?.slug?.current}
                            alt={post?.slug?.current}
                            src={urlFor(post.cover).url()}
-                           className=" duration-300 z-0 relative aspect-square md:hover:scale-95 object-cover w-full h-full "
+                           className=" duration-300 z-0 relative aspect-square md:hover:scale-95 object-cover w-full h-[250px] sm:h-[500px] "
                            effect="blur"
                         />
                      </div>
