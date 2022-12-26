08afb5cb5801dcfb76ee3ab4e8e5160418e9d132
Homero José <96498455+rox651@users.noreply.github.com>
2022-12-26 12:50:46 -0500

    navbar link fixed

--- src/components/layout/NavBarLink.jsx
+++ src/components/layout/NavBarLink.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export const NavBarLink = ({ link: { name, href } }) => {
    return (
       <Link
-         className=" order-2 lg:order-1 relative overflow-hidden hover:before:opacity-100 hover:before:translate-x-0 before:duration-200 before:transition-all before:-translate-x-full before:opacity-0 before:content-[''] before:absolute  before:block before:w-full before:h-[2px] before:bg-white before:bottom-0"
          to={href}
+         className=" order-2 lg:order-1 relative overflow-hidden hover:before:opacity-100 hover:before:translate-x-0 before:duration-200 before:transition-all before:-translate-x-full before:opacity-0 before:content-[''] before:absolute  before:block before:w-full before:h-[2px] before:bg-white before:bottom-0"
       >
          {name}
       </Link>
