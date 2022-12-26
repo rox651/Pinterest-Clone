f187d21052d133dd5856cfa2d1c7972d61b6ae75
Homero José <96498455+rox651@users.noreply.github.com>
2022-12-26 12:37:31 -0500

    post test

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,5 +1,8 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
+
+import PlaceholderImg from "../../assets/images-home/placeholder.webp";
+
 import { useCustomFetch } from "../../hooks/useCustomFetch";
 import { urlFor } from "../../lib";
 import { Loading } from "./Loading";
@@ -13,15 +16,18 @@ export const Posts = () => {
    return (
       <>
          <section className="relative  min-h-screen ">
-            <div className="px-5  max-w-7xl mx-auto   grid grid-cols-2 lg:grid-cols-3   gap-3 ">
+            <div className="px-5  max-w-7xl mx-auto   grid sm:grid-cols-2 lg:grid-cols-3   gap-3 ">
                {data.map(post => {
                   return (
-                     <div key={post?.slug?.current} className="overflow-hidden w-full h-full">
+                     <div
+                        key={post?.slug?.current}
+                        className="overflow-hidden w-full h-full md:hover:scale-95 duration-300"
+                     >
                         <LazyLoadImage
                            alt={post?.slug?.current}
                            src={urlFor(post.cover).url()}
-                           className=" duration-300 z-0 relative aspect-square md:hover:scale-95 object-cover w-full h-[250px] sm:h-[500px] "
-                           effect="blur"
+                           placeholderSrc={PlaceholderImg}
+                           className="relative aspect-square object-cover w-full sm:h-[500px] "
                         />
                      </div>
                   );
